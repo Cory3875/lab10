af6b20e13c935352e6745bb636df7a582aaab8b3
Cory3875 <ccadle99@gmail.com>
2014-10-21 15:37:31 -0500

    Fourth Commit

--- 70-100pt.py
+++ 70-100pt.py
@@ -25,17 +25,18 @@ drawpad.grid(row=0, column=1)
 #70 Pt 
 #Body and roof of house
 
-Rectangle = drawpad.create_rectangle(250,600,550,250)
+Rectangle = drawpad.create_rectangle(250,600,550,250,fill = 'red')
 line = drawpad.create_line(250,250,400,100)
 line2 = drawpad.create_line(400,100,550,250)
 
 #80 Pt
 #Windows and a door
 
-Rectangle2 = drawpad.create_rectangle(375,600,450,475)
-Rectangle3 = drawpad.create_rectangle(260,500,360,390)
-Rectangle4 = drawpad.create_rectangle(440,360,540,260)
-
+Rectangle2 = drawpad.create_rectangle(375,600,450,475,fill = 'brown')
+Rectangle3 = drawpad.create_rectangle(260,500,360,390,fill = 'blue')
+Rectangle4 = drawpad.create_rectangle(440,360,540,260,fill = 'blue')
+Rectangle7 = drawpad.create_rectangle(275,350,375,275,fill = 'blue')
+Rectangle8 = drawpad.create_rectangle(460,500,540,400,fill = 'blue')
 #90 Pt
 #Door Handle and Chimney
 
@@ -44,9 +45,11 @@ line3 = drawpad.create_line(450,150,450,100)
 line4 = drawpad.create_line(450,100,500,100)
 line5 = drawpad.create_line(500,100,500,200)
 
+#100 Pt
+#Green Grass and Red House
 
-
-
+Rectangle5 = drawpad.create_rectangle(0,600,250,575,fill = 'green')
+Rectangle6 = drawpad.create_rectangle(550,600,800,575,fill = 'green')
 
 
 
